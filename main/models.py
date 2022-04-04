961d50398dde32d169e90becfb3b62a3d78f2daf
Aze <Zahar70070@gmail.com>
2022-04-05 01:41:46 +0300

    name

--- main/models.py
+++ main/models.py
@@ -1,3 +1,15 @@
+from turtle import title
 from django.db import models
 
+
+class Task(models.Model):
+    title = models.CharField('Название', max_length=50)
+    task = models.TextField('Описание')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Новость дня'
+        verbose_name_plural = 'Новости дня'
 # Create your models here.
